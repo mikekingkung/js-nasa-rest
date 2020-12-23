9cafaaf5c09cbad06194d61224a6080073ea4c79
Mike King <michael.king4@virginmedia.com>
2020-12-23 21:21:26 +0000

    Changes to work with images and video

---
+++ to-do/src/components/Resource.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Resource = (props) => {
+    if (props) {
+        <div>
+
+        </div>
+    }
+    else {
+        <div></div>
+    }
+}
+
+export default Resource;
